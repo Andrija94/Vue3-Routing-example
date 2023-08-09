f5fec453be2e13057cddea98beebd17b58dc27e5
Andrija94 <andrija.velickovic94@gmail.com>
2023-08-09 11:10:44 +0200

    add basic routing of the web app

---
+++ ninja-jobs/src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid magni magnam dolorum possimus. Ratione quae vel, quisquam recusandae inventore odit quaerat consectetur esse corporis aut, id laborum commodi eos perspiciatis. Iure neque dolor, commodi velit natus deserunt veritatis excepturi expedita? Fugit dolor ut, adipisci dolores tempore consequuntur modi iusto totam reiciendis unde praesentium, voluptates voluptatum quia laborum labore, corrupti dolore asperiores inventore voluptatem alias? Aperiam veniam facilis voluptatem, sit libero at dolore iste quae sapiente architecto ab doloremque laudantium eaque vel repellat sint doloribus dignissimos dolor praesentium, fuga sunt dicta, recusandae vero cumque. Exercitationem sunt, commodi esse voluptatem debitis recusandae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid magni magnam dolorum possimus. Ratione quae vel, quisquam recusandae inventore odit quaerat consectetur esse corporis aut, id laborum commodi eos perspiciatis. Iure neque dolor, commodi velit natus deserunt veritatis excepturi expedita? Fugit dolor ut, adipisci dolores tempore consequuntur modi iusto totam reiciendis unde praesentium, voluptates voluptatum quia laborum labore, corrupti dolore asperiores inventore voluptatem alias? Aperiam veniam facilis voluptatem, sit libero at dolore iste quae sapiente architecto ab doloremque laudantium eaque vel repellat sint doloribus dignissimos dolor praesentium, fuga sunt dicta, recusandae vero cumque. Exercitationem sunt, commodi esse voluptatem debitis recusandae.</p>
+
+  </div>
+</template>
